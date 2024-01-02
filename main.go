64cc7a7a2e4a11ccb8ab366faf8f7aa9ef8b8c94
bluestar_ <canikissfemboy@gmail.com>
2024-01-03 01:41:13 +0700

    fix cors issues

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"math/rand"
 	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,11 +13,16 @@ func main() {
 	loadDataFromJSONFile()
 
 	router := gin.Default()
-
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:  []string{"*"},
+		AllowMethods:  []string{"GET"},
+		AllowHeaders:  []string{"Content-Type"},
+		ExposeHeaders: []string{"Content-Length"},
+	}))
 	router.POST("/api/add", addURL)
 	router.GET("/:urlID", redirectToLongURL)
 
-	router.Run(":8080")
+	router.Run(":8653")
 
 	saveDataToJSONFile()
 }
